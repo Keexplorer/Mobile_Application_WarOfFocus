f219f797145505975b6c497c26036cbef9794e17
orhan <orhangun009@gmail.com>
2025-04-23 23:41:46 +0300

    profile

--- app/src/main/java/com/example/focusapp2/MainActivity.kt
+++ app/src/main/java/com/example/focusapp2/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.focusapp2
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +14,9 @@ class MainActivity : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //       }
+
+
+
     }
+
 }
